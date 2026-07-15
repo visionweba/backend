187b640ab90b2d00f71bdbb1473d3bebfe95be58
Vision Web <visionwebaccess@gmail.com>
2026-07-16 02:35:52 +0530

    done

--- server.js
+++ server.js
@@ -14,12 +14,14 @@ connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://visionedgebusniess.com",
+    "https://www.visionedgebusniess.com"
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
